9df84df5c3e3288dea92281648ef2ccadbd47aa9
tobep <tobepc@mail.uc.edu>
2023-09-15 15:02:41 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // declaring variables
+        int intOperandA = 10;
+        int intOperandB = 5;
+        int intSum, intProduct, intDifference, intQuotient, intModulo;
+
+        //declaring double variants
+        double doubleOperandA = 3.50;
+        double doubleOperandB = 2.25;
+        double doubleSum, doubleProduct, doubleDifference, doubleQuotient;
+// declaring more variables
+        intSum = intOperandA + intOperandB;
+        intProduct = intOperandA * intOperandB;
+        intDifference = intOperandA - intOperandB;
+        intQuotient = intOperandA / intOperandB;
+        intModulo = 0;
+// declaring variables
+        doubleSum = doubleOperandA + doubleOperandB;
+        doubleProduct = doubleOperandA * doubleOperandB;
+        doubleDifference = doubleOperandA - doubleOperandB;
+        doubleQuotient = doubleOperandA / doubleOperandB;
+// declaring printed statements of variables
+        System.out.println("Int Sum: " + intSum);
+        System.out.println("Int Product: " + intProduct);
+        System.out.println("Int Difference: " + intDifference);
+        System.out.println("Int Quotient: " + intQuotient);
+        System.out.println("Int Modulo: " + intModulo);
+
+        System.out.println("Double Sum: " + doubleSum);
+        System.out.println("Double Product: " + doubleProduct);
+        System.out.println("Double Difference: " + doubleDifference);
+        System.out.println("Double Quotient: " + doubleQuotient);
+    }
+}
